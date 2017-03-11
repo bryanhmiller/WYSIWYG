0bef021395114d10a5e464056b99aa61e13a0d62
bryanhmiller <bryankalos@hotmail.com>
2017-03-11 17:17:55 -0600

    Completed everything up to binding the text input to the bio.

--- wysiwyg.js
+++ wysiwyg.js
@@ -18,7 +18,7 @@ var presidentialOutput = document.getElementById("presidential-container");
 //     }
 var presidentialArray = [
 {
-  title: "First President",
+  title: "First-President",
   name: "George Washington",
   bio: "George Washington was born on February 22, 1732, in Westmoreland County, Virginia. Washington served as a general and commander-in-chief of the colonial armies during the American Revolution, and later became the first president of the United States, serving from 1789 to 1797.",
   image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTIwNjA4NjM0MDA1MzkwODYw/george-washington-9524786-1-402.jpg",
@@ -28,7 +28,7 @@ var presidentialArray = [
   }
 },
 {
-  title: "Second President",
+  title: "Second-President",
   name: "John Adams",
   bio: "John Adams was born on October 30, 1735, in Braintree (now Quincy), Massachusetts. His father, John Adams Sr., was a farmer, a Congregationalist deacon and a town councilman, and was a direct descendant of Henry Adams, a Puritan who emigrated from England to the Massachusetts Bay Colony in 1638.",
   image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxMjc3MzIxNzQx/john-adams-37967-1-402.jpg",
@@ -38,7 +38,7 @@ var presidentialArray = [
   }
 },
 {
-  title: "Third President",
+  title: "Third-President",
   name: "Thomas Jefferson",
   bio: "Thomas Jefferson was born on April 13, 1743, in Shadwell, Virginia. He was a draftsman of the U.S. Declaration of Independence; the nation's first secretary of state (1789-94); second vice president (1797-1801); and, as the third president (1801-09), the statesman responsible for the Louisiana Purchase.",
   image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU1MDEwMjQ4MjA3/thomas-jefferson-9353715-1-402.jpg",
@@ -48,7 +48,7 @@ var presidentialArray = [
   }
 },
 {
-  title: "Fourth President",
+  title: "Fourth-President",
   name: "James Madison",
   bio: "Born on March 16, 1751, in Port Conway, Virginia, James Madison wrote the first drafts of the U.S. Constitution, co-wrote the Federalist Papers and sponsored the Bill of Rights. ... He died on June 28, 1836, at the Montpelier estate in Orange County, Virginia.",
   image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNjEwMzI1MDA2/james-madsion-9394965-1-402.jpg",
@@ -58,7 +58,7 @@ var presidentialArray = [
   }
 },
 {
-  title: "Fifth President",
+  title: "Fifth-President",
   name: "James Monroe",
   bio: "Born on April 28, 1758, in Westmoreland County, Virginia, James Monroe fought under George Washington and studied law with Thomas Jefferson. He was elected the fifth president of the United States in 1817.",
   image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxNzI2OTY0MjM3/james-monroe-9412098-1-402.jpg",
@@ -95,8 +95,8 @@ function populateDemPresidents() {
 // 4. Create a DOM element for each of the objects inside the container. Style your person 
 // elements however you like.
       presidentialInfo += `<person class="col-sm-6 col-md-3 thumbnail">`;
-      presidentialInfo += `<header>${currentPresident.name} ${currentPresident.title}</header>`;
-      presidentialInfo += `<section>${currentPresident.bio} <img src="${currentPresident.image}"/></section>`;
+      presidentialInfo += `<header>${currentPresident.name}, ${currentPresident.title}</header>`;
+      presidentialInfo += `<section id="president-bio">${currentPresident.bio} <img src="${currentPresident.image}"/></section>`;
       presidentialInfo += `<footer>${currentPresident.lifespan.birth} to ${currentPresident.lifespan.death}</footer>`;
       presidentialInfo += `</person>`;
   }
@@ -106,14 +106,23 @@ function populateDemPresidents() {
 function presidentialEvent() {
 // 8. When you click on one of the person elements, a dotted border should appear around it.
   if (event.target.tagName === "PERSON") {
-    console.log(event.target.tagName);
     event.target.classList.add("dotted-border");
 
 // 9. When you click on one of the person elements, the text input should immediately gain 
 // focus so that you can start typing.
-  
+    presidentialInput.focus();
+    presidentialInput.addEventListener("keyup", bindingToBio);
   } 
 }
+
+// 10. When there is a highlighted person element, and you begin typing in the input box, 
+// the person's biography should be immediately bound to what you are typing, letter by 
+// letter.
+function bindingToBio() {
+  var additionalBio = document.getElementById("president-bio");
+  additionalBio.innerHTML = presidentialInput.value; 
+}
+
 populateDemPresidents();
 
 presidentialOutput.addEventListener("click", presidentialEvent);
