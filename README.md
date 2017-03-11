3693ff6375e64a9a9687d655a4a75840453b5821
bryanhmiller <bryankalos@hotmail.com>
2017-03-11 12:09:16 -0600

    Created all of the files for project, began populating objects in array.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # WYSIWYG
+
+1. Create an array of objects that represents famous people (see structure below).
+		Object structure
+
+		{
+		  title: "Samurai",
+		  name: "Tomoe Gozen",
+		  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+		  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
+		  lifespan: {
+		    birth: 1747,
+		    death: 1797
+		  }
+		}
+2. Create a text input in your DOM.
+3. Beneath that, create a container, block element in your DOM.
+4. Create a DOM element for each of the objects inside the container. Style your person elements however you like.
+5. For every even numbered element, have a light yellow background.
+6. For every odd numbered element, have a light blue background.
+7. Each element's DOM structure should be as shown below.
+		Sample Person Element & Children
+
+		This is just a sample and is not what your final HTML will look like. You need to add all the appropriate id and class attribute needed to make it work as intended.
+
+		<!-- ///
+		  HTML is a flexible specification. I just made up the person tag,
+		  and that's perfectly legal. You can then style it with CSS however
+		  you wish.
+		/// -->
+		<person>
+		  <header>Name and title go here</header>
+		  <section>Bio and image go here</section>
+		  <footer>Lifespan info goes here</footer>
+		</person>
+
+8. When you click on one of the person elements, a dotted border should appear around it.
+9. When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
+10. When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
+11. When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
+
---
+++ wysiwyg.js
@@ -0,0 +1,50 @@
+{
+  title: "First President",
+  name: "George Washington",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTIwNjA4NjM0MDA1MzkwODYw/george-washington-9524786-1-402.jpg"
+  lifespan: {
+    birth: 1732,
+    death: 1799
+  }
+}
+{
+  title: "Second President",
+  name: "John Adams",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxMjc3MzIxNzQx/john-adams-37967-1-402.jpg"
+  lifespan: {
+    birth: 1735,
+    death: 1826
+  }
+}
+{
+  title: "Third President",
+  name: "Thomas Jefferson",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU1MDEwMjQ4MjA3/thomas-jefferson-9353715-1-402.jpg"
+  lifespan: {
+    birth: 1743,
+    death: 1826
+  }
+}
+{
+  title: "Fourth President",
+  name: "James Madison",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNjEwMzI1MDA2/james-madsion-9394965-1-402.jpg"
+  lifespan: {
+    birth: 1751,
+    death: 1836
+  }
+}
+{
+  title: "Fifth President",
+  name: "James Monroe",
+  bio: "Born on April 28, 1758, in Westmoreland County, Virginia, James Monroe fought under George Washington and studied law with Thomas Jefferson. He was elected the fifth president of the United States in 1817.",
+  image: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxNzI2OTY0MjM3/james-monroe-9412098-1-402.jpg"
+  lifespan: {
+    birth: 1758,
+    death: 1831
+  }
+}
